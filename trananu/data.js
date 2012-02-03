e8b62db0a112ff0ed0867bc693ba84fe1c21754f
CB <claes.bjorklund@gmail.com>
2012-02-03 14:59:26 +0100

    Added text

--- trananu/data.js
+++ trananu/data.js
@@ -1,27 +1,98 @@
 var data_sv = { 111: { 'ingress' : 'hej hej',
 		       'pass1'   : {'rubrik': 'Rub1',
-				    'txt'   : "Uppvärmning: 5 min lättjogg/gå, (lätt tänjning 3 min) spring eller jogga 30 sekunder, gå 2min. Upprepa 3 gånger, gå/jogga 3 min"},
+				    'txt'   : "Uppvärmning: 5 min lättjogg/gå, (lätt tänjning 3 min) spring eller jogga 30 sekunder, gå 2min. Upprepa 3 gånger, gå/jogga 3 minnnn <br> Övning 1, bla bla <br> Övning 2, bla ba"},
 		       'pass2' : {'rubrik' : 'Rub2', 
-				  'txt'    : 'Lite Lättare träning',
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 },
+		       'pass3' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
 				 }
 		     },
-	     112: {  1: "uppvärmning: 5 min lättjogg, tänj/stretch 5 min, löpskolning 5-10min, 5x100m löpning, vila gå tillbaka till startpunkten, hopprep 10 min ,nedjogg 5 min, stretch",
-		     2: 'Lite Lättare träning'
-		  },
-	     113: {  1: "Uppvärmning: 10 min snabb gång + avlsutande 2 min jogg, 5 min tänjningar, 10 min löpskolning, 5x \"gissa 200\", bestäm en tid du vill ha på 200m och försök att pricka den, vila 5 min mellan loppen, nedjogg, stretch",
-		     2: 'Lite Lättare träning'
-		  },
-	     
-	     211: {1:'Medel träning'
-		  },
-	     212: {1:'Medel träning lite mer'
-		  },
-	     213: {1:'Medel träning mycket mer'
-		  },
-	     311: {1:'Svår träning'
-		  },
-	     312: {1:'Svår träning lite mer'
-		  },
-	     313: {1:'Svår träning mycket mer'
-		  }
+	     112: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Uppvärmning: 5 min lättjogg/gå, (lätt tänjning 3 min) spring eller jogga 30 sekunder, gå 2min. Upprepa 3 gånger, gå/jogga 3 minnnn <br> Övning 1, bla bla <br> Övning 2, bla ba"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+	     113: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Uppvärmning: 5 min lättjogg/gå, (lätt tänjning 3 min) spring eller jogga 30 sekunder, gå 2min. Upprepa 3 gånger, gå/jogga 3 minnnn <br> Övning 1, bla bla <br> Övning 2, bla ba"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+	     211: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Uppvärmning: 5 min lättjogg/gå, (lätt tänjning 3 min) spring eller jogga 30 sekunder, gå 2min. Upprepa 3 gånger, gå/jogga 3 minnnn <br> Övning 1, bla bla <br> Övning 2, bla ba"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+	     212: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Medel träning lite mer"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+	     213: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Medel träning mycket mer"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+	     311: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Svår träning"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+	     312: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Svår träning lite mer"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+	     313: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'Rub1',
+				    'txt'   : "Svår träning mycket mer <br> Övning 1, bla bla <br> Övning 2, bla ba"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Lite Lättare träning <br> Övning 1,bla <br> Övning 2, bla',
+				 }
+		     },
+		'niva' :[[-1,'Välj nivå'],
+			[1,'Lätt'],
+			[2,'Medel'],
+			[3,'Svår']
+		       ],
+		'form': [[-1,'Välj form'],
+			 [1,'Löpning'],
+			 [2,'Armar'],
+			 [3,'Mage']
+			],
+		'tid':[
+		    [-1,'Välj tid'],
+		    [1,'15-20 min'],
+		    [2,'35-40 min'],
+		    [3,'55-60 min']
+		],
+		'head':'Träna Nuu',
+	      };
+
+
+var data_en = { 111: { 'ingress' : 'hej hej',
+		       'pass1'   : {'rubrik': 'head 1',
+				    'txt'   : "English text here"},
+		       'pass2' : {'rubrik' : 'Rub2', 
+				  'txt'    : 'Engelska',
+				 },
+		       'pass3' : {'rubrik' : 'HEad 2', 
+				  'txt'    : 'ENg',
+				 }
+		     },
+
 	      };
