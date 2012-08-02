f716becf8272173f8c74081a05b8babcf2b2c425
CB <claes.bjorklund@gmail.com>
2012-08-02 21:58:18 +0200

    new version

--- val/ledamot.js
+++ val/ledamot.js
@@ -1,28 +1,85 @@
-function valda(liströst, kandidat,mandat) {
+function valda(liströst, kandidat2,mandat) {
+    var kanditat= new cloneObject(kandidat2);
     var första={}
-
-    var platstal=0;
+    var topp={};
+    var platstal={};
+    var röstetal={};
     var lista={};    
+    var R={};
+    var ledamot=[];
+// init
+    for (var L in kandidat) {
+	var först=kandidat[L].shift();
+	if (typeof topp[först] == "undefined") {
+	    topp[först]=[];
+	    R[först]=0;
+	    röstetal[först]=0;
+	    platstal[först]=0;
+	}
+	topp[först].push(L);
+	R[först] += liströst[L];
+	röstetal[först] += liströst[L];
+
+    }
+    console.log(topp);
+	    var list=kandidat['L1'];
+   var först=list.shift();
+    console.log(list);
+    console.log(först);
 
     for (var i=0;  i<mandat; i++) {
-	var R={};
-	for (L in kandidat) {
-	    console.log(L);
-	    if (kandidat[L].length) {
-		första[L]=kandidat[L].shift();
-		console.log(första[L]);
-		R[första[L]]=liströst[L]/(1+platstal);
+	var max= maxjmf(R);
+	ledamot.push(max);
+	console.log(i + ' max=' + max);
+	console.log(topp[max]);
+	var vv=topp[max];
+	console.log(vv);
+	for (var L in vv) {
+	    console.log( vv[L] + '=' + kandidat[vv[L]]);
+	    
+	    var list=kandidat[vv[L]];
+	console.log(list.length);
+	    if(list.length) {
+		console.log(list);
+		var först=list.shift();
+		console.log('först' + ' =' + först);
+		if (typeof topp[först] == "undefined") {
+		    topp[först]=[];
+		    R[först]=0;
+		    röstetal[först]=0;
+		    platstal[först]=0;
+		}
+		topp[först].push(vv[L]);
+		for (var L in topp[först]) {
+		    console.log('LL ' + först + ' ' + topp[först][L]);
+		    röstetal[först] += liströst[topp[först][L]];		    
+		}
+		console.log(först);
+		console.log('röstetal');
+		console.log(röstetal);
+		platstal[först] += röstetal[först]/R[max];
+		console.log(platstal);
+		R[först]=röstetal[först]/(platstal[först]+1);
 
+		console.log('R');
 		console.log(R);
 
+		
 	    }
-	    var max= maxjmf(R);
-		console.log('max');
+		console.log('MAX');
 		console.log(max);
+		delete topp[max]; 
+		delete röstetal[max]; 
+		delete platstal[max];
+		delete R[max];
+		console.log(topp);
+		console.log(R);
+	    
 	}
     }
- 
-
+	
+	console.log(ledamot);
+    
 }
 
 function maxjmf(jmf){
@@ -41,7 +98,23 @@ function maxjmf(jmf){
     }
 //    return maxparti;
     }
-var liströst={'L1':123,'L2':213};
-var kandidat={'L1':['A','B','C'],'L2':['F','B','C']};
 
+function cloneObject(source) {
+    for (i in source) {
+        if (typeof source[i] == 'source') {
+            this[i] = new cloneObject(source[i]);
+        }
+        else{
+            this[i] = source[i];
+	}
+    }
+}
+ 
+var obj1= {bla:'blabla',foo:'foofoo',etc:'etc'};
+ 
+var obj2= new cloneObject(obj1);
+
+var liströst={'L1':3898,'L2':1478,'L3':3473};
+var kandidat={'L1':['A','B','C'],'L2':['A','F','G'],'L3':['A','H','I']};
+console.log(kandidat);
 valda(liströst,kandidat,5);
